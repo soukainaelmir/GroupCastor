e83eab17c1ed08f31d3579555bba18839047acca
soukaina <soukaina.elmir.93@gmail.com>
2023-01-01 20:45:57 -0700

    updated details

--- src/utils/MainUtils.java
+++ src/utils/MainUtils.java
@@ -1,11 +1,6 @@
 package utils;
 
 public class MainUtils {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-
-    public void coolMethod(){
-
-    }
+    //test
+    // practice
 }
