ded9c2c238ee4126feaadf774590a21188a1b7f6
Abdellatif Zouliga <zuuliga@gmail.com>
2023-01-06 18:33:25 -0500

    week 3 tasks

---
+++ src/week_3/ReverseNegativeNums.java
@@ -0,0 +1,41 @@
+package week_3;
+
+
+import java.util.Random;
+
+public class ReverseNegativeNums {
+
+    public static void main(String[] args) {
+
+        //System.out.println(reverseNegativeNumbers(-369));
+
+        // generate random numbers
+        Random random = new Random();
+
+
+        for (int i = 0; i < 10; i++) {
+            int nums = -100+ random.nextInt((150) );
+            System.out.println(nums + " : " +reverseNegativeNumbers(nums));
+        }
+
+    }
+
+    private static int reverseNegativeNumbers(int number) {
+
+        if (number < 0) {
+            String str = Math.abs(number) + "";
+            String[] array = str.split("");
+            String reversed = "";
+
+            for (int i = array.length - 1; i >= 0; i--) {
+                reversed += array[i];
+            }
+            return Integer.parseInt(reversed);
+        }
+        return number;
+
+
+    }
+
+
+}
